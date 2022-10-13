af023c2177a2a013d56494dd7d83907183589a02
Mac <mac@Mac.local>
2022-10-13 08:44:33 +0200

    installing react router dom @6 and changing the route component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
@@ -8,29 +8,28 @@ import ProductDetails from './pages/ProductDetails'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <nav>
           <h1>The Ninja Clothing Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/products">Products</Link>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/products'>Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/test' element={(
+            <div>
+              <h2>Test </h2><p>Hello!</p>
+            </div>
+          )} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
